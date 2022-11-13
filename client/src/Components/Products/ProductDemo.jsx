6959e8e1b831273aefe124bc98c2ea15e96ce667
avishay avraham <avishayavraham555@gmail.com>
2022-11-13 16:28:19 +0200

    product style

--- client/src/Components/Products/ProductDemo.jsx
+++ client/src/Components/Products/ProductDemo.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import { RadioGroup } from "@headlessui/react";
 
-// function classNames(...classes) {
-//   return classes.filter(Boolean).join(" ");
-// }
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 export default function ProductDemo({ product }) {
-//   const [selectedSize, setSelectedSize] = useState(product.sizes[2]);
+  const [selectedSize, setSelectedSize] = useState(product.sizes[2]);
 
   return (
+    
     <div className=" flex justify-center mt-3">
       <div className="container border border-[black] flex flex-col sm:flex-row justify-evenly p-7">
         {/* left side */}
@@ -25,7 +26,7 @@ export default function ProductDemo({ product }) {
           </div>
 
           {/* Sizes */}
-          {/* <div className="mt-10">
+          <div className="mt-10">
             <div className="flex items-center justify-between">
               <h4 className="text-sm font-medium text-gray-900">Size</h4>
               <a
@@ -104,7 +105,7 @@ export default function ProductDemo({ product }) {
                 ))}
               </div>
             </RadioGroup>
-          </div> */}
+          </div>
 
           {/* button */}
           <button
@@ -116,5 +117,24 @@ export default function ProductDemo({ product }) {
         </div>
       </div>
     </div>
+
+    // new demo
+//  <div className="flex justify-center ">   
+// <div className = "flex sm:flex-row flex-col items-center w-full max-w-xl bg-[#aaa] rounded-lg shadow-md border-[#2d2d2d] border-solid">
+//     <a href="#">
+//         <img className = "p-8 rounded-t-lg" src={product.image} alt="product image"/>
+//     </a>
+//     <div className = "px-5 pb-5">
+//         <a href="#">
+//             <h5 className = "text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{product.title}</h5>
+//         </a>
+//         <div className = "flex justify-between items-center">
+//             <span className = "text-3xl font-bold text-gray-900 dark:text-white">{product.price}$</span>
+//             <a href="#" className = "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+//         </div>
+//     </div>
+// </div>
+// </div>
+
   );
 }
