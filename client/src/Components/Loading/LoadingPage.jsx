64d57009a8023e3f6a106665bce00572691f3358
avishay avraham <avishayavraham555@gmail.com>
2022-11-28 13:08:30 +0200

    animation

--- client/src/Components/Loading/LoadingPage.jsx
+++ client/src/Components/Loading/LoadingPage.jsx
@@ -1,16 +1,7 @@
 import React from "react";
 import Template from "./Template";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 
-export default function LoadingPage({pageUrl}) {
-  let navigate = useNavigate();
-
-  useEffect(() => {
-    setTimeout(() => {
-      navigate(pageUrl);
-    }, "4000");
-  }, []);
+export default function LoadingPage() {
 
   return (
     <div style={{ background: " #141414", height: "100vh" }}>
--- client/src/Components/Products/ProductsTemplate.jsx
+++ client/src/Components/Products/ProductsTemplate.jsx
@@ -2,13 +2,17 @@ import axios from "axios";
 import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import LoadingPage from "../Loading/LoadingPage";
 
 export default function ProductTemplate() {
+  const [loading, setLoading] = useState(false)
   const [productsData, setProductsData] = useState([]);
 
   // function tpo get data using axios
   const getData = async () => {
+    setLoading(true);
     const { data } = await axios.get("https://nice-ox-windbreaker.cyclic.app/api/products");
+    setLoading(false)
     setProductsData(data);
   };
 
@@ -18,7 +22,8 @@ export default function ProductTemplate() {
 
   return (
     <div className="bg-[#aaa] opacity-60 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 rounded-xl">
-      {productsData?.map((product, i) => (
+     {loading ? <LoadingPage/> :
+      productsData?.map((product, i) => (
         <Link key={i} to={`/product/${product._id}`} className="group m-4">
           <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg xl:aspect-w-7 xl:aspect-h-8">
             <img
